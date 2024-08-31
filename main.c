fa23e46501cbc41e132342664cf88d25aeb331de
gabrielcorreiapontes <80489162+gabrielcorreiapontes@users.noreply.github.com>
2024-08-30 22:37:52 -0300

    Update main.c
    base 8,

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+
 void decimalParaBinario(int numero) {
     int binario[32]; 
     int i = 0;
 
-  
-    printf("Passo 1: Dividir %d por 2 até o quociente ser 0 e armazenar os restos:\n", numero);
+    
+    printf("Passo 1 (Base 2): Dividir %d por 2 até o quociente ser 0 e armazenar os restos:\n", numero);
 
     while (numero > 0) {
         binario[i] = numero % 2;
@@ -16,10 +17,35 @@ void decimalParaBinario(int numero) {
     }
 
     
-    printf("Passo 2: Inverter a ordem dos restos:\n");
+    printf("Passo 2 (Base 2): Inverter a ordem dos restos:\n");
     printf("Binário: ");
     for (int j = i - 1; j >= 0; j--) {
         printf("%d", binario[j]);
+    }
+    printf("\n");
+}
+
+
+void decimalParaOctal(int numero) {
+    int octal[32]; 
+    int i = 0;
+
+    
+    printf("Passo 1 (Base 8): Dividir %d por 8 até o quociente ser 0 e armazenar os restos:\n", numero);
+
+    while (numero > 0) {
+        octal[i] = numero % 8;
+        printf("%d %% 8 = %d (resto)\n", numero, octal[i]);
+        numero = numero / 8;
+        printf("%d / 8 = %d (novo número)\n", numero * 8 + octal[i], numero); 
+        i++;
+    }
+
+    
+    printf("Passo 2 (Base 8): Inverter a ordem dos restos:\n");
+    printf("Octal: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", octal[j]);
     }
     printf("\n");
 }
@@ -31,6 +57,7 @@ int main() {
     scanf("%d", &numero);
 
     decimalParaBinario(numero);
+    decimalParaOctal(numero);
 
     return 0;
 }
