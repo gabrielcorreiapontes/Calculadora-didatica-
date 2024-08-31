1bb313cfa6e52f40c3210a38dc667319c6402cb7
gabrielcorreiapontes <80489162+gabrielcorreiapontes@users.noreply.github.com>
2024-08-30 22:44:34 -0300

    Update main.c
    conversão para BCD

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ void decimalParaBinario(int numero) {
     int binario[32]; 
     int i = 0;
 
-    
+  
     printf("Passo 1 (Base 2): Dividir %d por 2 até o quociente ser 0 e armazenar os restos:\n", numero);
 
     while (numero > 0) {
@@ -61,7 +61,7 @@ void decimalParaHexadecimal(int numero) {
     while (numero > 0) {
         int resto = numero % 16;
 
-        
+       
         if (resto < 10) {
             hexadecimal[i] = 48 + resto; 
         } else {
@@ -70,7 +70,7 @@ void decimalParaHexadecimal(int numero) {
 
         printf("%d %% 16 = %c (resto)\n", numero, hexadecimal[i]);
         numero = numero / 16;
-        printf("%d / 16 = %d (novo número)\n", numero * 16 + resto, numero); 
+        printf("%d / 16 = %d (novo número)\n", numero * 16 + resto, numero);
         i++;
     }
 
@@ -79,6 +79,34 @@ void decimalParaHexadecimal(int numero) {
     printf("Hexadecimal: ");
     for (int j = i - 1; j >= 0; j--) {
         printf("%c", hexadecimal[j]);
+    }
+    printf("\n");
+}
+
+
+void decimalParaBCD(int numero) {
+    int digito;
+    int bcd[4]; 
+
+    printf("Passo 1 (BCD): Converter cada dígito decimal em seu equivalente binário:\n");
+
+    printf("BCD: ");
+    while (numero > 0) {
+        digito = numero % 10; 
+
+       
+        for (int i = 3; i >= 0; i--) {
+            bcd[i] = digito % 2;
+            digito = digito / 2;
+        }
+
+      
+        for (int i = 0; i < 4; i++) {
+            printf("%d", bcd[i]);
+        }
+        printf(" "); 
+
+        numero = numero / 10;  
     }
     printf("\n");
 }
@@ -92,6 +120,7 @@ int main() {
     decimalParaBinario(numero);
     decimalParaOctal(numero);
     decimalParaHexadecimal(numero);
+    decimalParaBCD(numero);
 
     return 0;
 }
